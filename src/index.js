756253b51ae618801e10b59aad78704e1c2417b8
mateaz <matea_3@hotmail.com>
2021-07-21 18:22:09 +0200

    SIngle page done

--- src/index.js
+++ src/index.js
@@ -6,3 +6,9 @@ heading.render();
 
 const helloWorldButton = new HelloWorldButton;
 helloWorldButton.render();
+
+if(process.env.NODE_ENV === 'production') {
+    console.log('Production mode')
+} else if(process.env.NODE_ENV === 'development') {
+    console.log('Production mode')
+}
---
+++ webpack.dev.config.js
@@ -0,0 +1,75 @@
+const path = require('path');
+const {CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, './dist'),
+        publicPath: ''
+    },
+    
+    mode: 'development',
+    devServer: {
+        contentBase:path.resolve(__dirname, './dist'),
+        index: 'index.html',
+        port: 9000,
+        writeToDisk: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpg)$/,
+                type: 'asset/resource'
+            },
+            {
+                test: /\.txt/,
+                type: 'asset/source'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader', 'css-loader'
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader', 'css-loader', 'sass-loader'
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/env'], 
+                        plugins: ['@babel/plugin-proposal-class-properties']
+                    }
+                }
+            },
+            {
+                test: /\.hbs$/,
+                use: [
+                    'handlebars-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin({
+            cleanOnceBeforeBuildPatterns: [
+                '**/*', 
+                path.join(process.cwd(), 'build/**/*')
+            ]
+        }),
+        new HtmlWebpackPlugin({
+            title: 'Hello World',
+            template: 'src/index.hbs',
+            description: 'Some'
+        }),
+    ]
+}
